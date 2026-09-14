e2525027226af62be574ab9350e5c5b493ed8d99
dorazzii <henrique.reis2@estudante.ifms.edu.br>
2026-09-14 21:02:29 +0000

    ajustando

---
+++ sw.js
@@ -0,0 +1,9 @@
+
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker ativado");
+
+});
