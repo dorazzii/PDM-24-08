abaaefb9184589e68751711a1289a988c1155cc0
dorazzii <henrique.reis2@estudante.ifms.edu.br>
2026-09-14 21:36:36 +0000

    com a alteracao do sw

--- sw.js
+++ sw.js
@@ -7,3 +7,16 @@ self.addEventListener("activate", event => {
     console.log("Service Worker ativado");
 
 });
+
+self.addEventListener("fetch", event => {
+console.log("Requisição:", event.request.url);
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.endsWith("/teste-sw")) {
+    event.respondWith(
+      new Response("Resposta criada pelo Service Worker!")
+    );
+    return;
+  }
+});
