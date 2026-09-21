33a3a7840e6f86767bdb53226b237dde6c3d7b74
dorazzii <henrique.reis2@estudante.ifms.edu.br>
2026-09-21 15:51:40 +0000

    sw pronto

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-
 self.addEventListener("install", event => {
+
     console.log("Service Worker instalado");
+
 });
 
 self.addEventListener("activate", event => {
+
     console.log("Service Worker ativado");
 
 });
 
 self.addEventListener("fetch", event => {
-console.log("Requisição:", event.request.url);
-});
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.url.endsWith("/teste-sw")) {
-    event.respondWith(
-      new Response("Resposta criada pelo Service Worker!")
-    );
-    return;
-  }
+    console.log("Requisição:", event.request.url);
+
+    if (event.request.url.endsWith("/teste-sw")) {
+
+        event.respondWith(
+            new Response("Resposta criada pelo Service Worker!")
+        );
+
+    }
+
 });
